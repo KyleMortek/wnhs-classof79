c576e393da576403ee824b1f2e11772324a3ac66
lightBulb17 <lightBulb17@github.com>
2019-02-28 18:09:45 -0500

    many updates

--- docs/resources/js/contact-form.js
+++ docs/resources/js/contact-form.js
@@ -1,34 +1,52 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  let name = e.target.elements.name.value;
-  let email = e.target.elements.email.value;
-  let message = e.target.elements.message.value;
+// document.getElementById('contact-form').addEventListener('submit', function (e) {
+//   e.preventDefault();
+//   let name = e.target.elements.name.value;
+//   let email = e.target.elements.email.value;
+//   let message = e.target.elements.message.value;
 
-  let Tbury = e.target.elements.Tbury.value;
-  let Irish = e.target.elements.Irishman.value;
-  let BT = e.target.elements.BRHT.value;
-  let Bisons = e.target.elements.Bisons.value;
+//   let Tbury = e.target.elements.Tbury.value;
+//   let Irish = e.target.elements.Irishman.value;
+//   let BT = e.target.elements.BRHT.value;
+//   let Bisons = e.target.elements.Bisons.value;
 
-  let data = {
-    name: name,
-    email: email,
-    message: message,
-    Tbury: Tbury,
-    Irishman: Irish,
-    BT: BT,
-    Bisons:Bisons 
-  };
-  var firebaseRef = firebase.database().ref();
-  firebaseRef.child("students").push(email).set(data);
-  // firebaseRef.child(email).set(data);
+//   let data = {
+//     name: name,
+//     email: email,
+//     message: message,
+//     Tbury: Tbury,
+//     Irishman: Irish,
+//     BT: BT,
+//     Bisons:Bisons 
+//   };
+//   var firebaseRef = firebase.database().ref();
+//   firebaseRef.child("students").push(email).set(data);
+//   // firebaseRef.child(email).set(data);
 
-  // var rootRef = firebase.database.ref();
-  // document.getElementById('submitBtn').addEventListener('click',function(e){
-    document.getElementById('contact-form').remove();
-    document.getElementById('Recieved').textContent="Submission Complete";
+//   // var rootRef = firebase.database.ref();
+//   // document.getElementById('submitBtn').addEventListener('click',function(e){
+//     document.getElementById('contact-form').remove();
+//     document.getElementById('Recieved').textContent="Submission Complete";
     
-  // });
-}); 
+//   // });
+// }); 
+
+// var firebaseRef = firebase.database().ref();
+// var urlRef = firebaseRef.child("students");
+// urlRef.once("value", function (snapshot) {
+//   snapshot.forEach(function (child) {
+//     console.log(
+//     ` ${child.key}: 
+//     Name:     ${child.val().name}
+//     Email:    ${child.val().email}
+//     Message:  ${child.val().message}
+//     ~~~~~~~~~~~~~Going to~~~~~~~~~~~ 
+//     Tewksbury:${child.val().Tbury}
+//     Irishman: ${child.val().Irishman}
+//     BRHT:     ${child.val().BT}
+//     Bisons:   ${child.val().Bisons}`
+//     );
+//   });
+// });
 document.getElementById('BRHT').addEventListener('change',function(e){
   e.preventDefault();
   e.target.checked;
@@ -48,24 +66,7 @@ document.getElementById('BisonsGame').addEventListener('change',function(e){
   e.preventDefault();
   e.target.checked;
   console.log(e.target.checked);
-});   
-var firebaseRef = firebase.database().ref();
-var urlRef = firebaseRef.child("students");
-urlRef.once("value", function (snapshot) {
-  snapshot.forEach(function (child) {
-    console.log(
-    ` ${child.key}: 
-    Name:     ${child.val().name}
-    Email:    ${child.val().email}
-    Message:  ${child.val().message}
-    ~~~~~~~~~~~~~Going to~~~~~~~~~~~ 
-    Tewksbury:${child.val().Tbury}
-    Irishman: ${child.val().Irishman}
-    BRHT:     ${child.val().BT}
-    Bisons:   ${child.val().Bisons}`
-    );
-  });
-});
+});    
 ////////////////////////////////////////
 ////////////////////////////////////////
 ////////////////////////////////////////
