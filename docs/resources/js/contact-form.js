610f49e76f5f120420ecf90a6b9a07057a87d57f
lightBulb17 <lightBulb17@github.com>
2019-04-14 14:29:47 -0400

    u
    u

--- docs/resources/js/contact-form.js
+++ docs/resources/js/contact-form.js
@@ -73,30 +73,7 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
       rootRef.child("headCount").set(headNum);
       document.getElementById('headCount').textContent = `head Count = ${headNum}`;
     });
-    // var nodemailer = require('nodemailer');
 
-    // var transporter = nodemailer.createTransport({
-    //   service: 'gmail',
-    //   auth: {
-    //          user: 'kody7178@gmail.com',
-    //          pass: 'Itskody18-'
-    //      }
-    // });
-    
-    // const mailOptions = {
-    //   from: 'kody7178@gmail.com', // sender address
-    //   to: 'mortek17@gmail.com', // list of receivers
-    //   subject: 'Subject of your email', // Subject line
-      
-    //   html: '<p>Your html here</p>'// plain text body
-    // };
-    
-    // transporter.sendMail(mailOptions, function (err, info) {
-    //   if(err)
-    //     console.log(err)
-    //   else
-    //     console.log(info);
-    // });
 
     
 
